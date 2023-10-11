607890011241d9d93707a66fa8bfb59ee67e8045
rubyrobin333 <kbeasl2@artic.edu>
2023-10-11 17:39:47 -0500

    leeee

---
+++ p5drawingbruh/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(800, 800)
+    background(220, 250, 255);
+}
+
+function draw(){
+    noStroke();
+    fill(150, 25, 175, 150)
+    ellipse(mouseX, mouseY, 30)
+  
+   
+
+}
+
+function mousePressed() {
+    background(220, 250, 255);
+
+}
