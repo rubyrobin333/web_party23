1f820f3b6882555dc7087d1dd610853423e7bbbd
rubyrobin333 <kbeasl2@artic.edu>
2023-10-11 20:47:02 -0500

    flexxxxx

--- p5drawingbruh/sketch.js
+++ p5drawingbruh/sketch.js
@@ -1,13 +1,15 @@
 function setup() {
     createCanvas(800, 800)
     background(220, 250, 255);
+    
 }
 
 function draw(){
     noStroke();
     fill(150, 25, 175, 150)
     ellipse(mouseX, mouseY, 30)
-  
+   
+    
    
 
 }
